eefe3cadc68dd4f75bc16f47b5430c3e4d3588a8
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-09-21 23:03:09 +0300

    Adding project Smart Ticketting with new html and utility file with basic condition

--- utility.js
+++ utility.js
@@ -2,4 +2,24 @@
 function addSeat(event){
     event.style.backgroundColor='lime';
     event.style.color='white';
+    const seatNo=event.innerText;
+    document.getElementById('noSeatLine').classList.add('hidden');
+    const newline=document.createElement('div');
+    newline.classList.add('flex')
+    newline.classList.add('justify-between')
+    newline.innerHTML=`
+    <p>${seatNo}</p>
+    <p>Economy</p>
+    <p>550</p>
+    `
+    document.getElementById('priceList').appendChild(newline)
+
+    let avilableNo=parseFloat(document.getElementById('availableNo').innerText)
+    avilableNo-=1;
+    document.getElementById('availableNo').innerText=avilableNo;
+
+    let selectNo=parseFloat(document.getElementById('selectNo').innerText)
+    selectNo+=1;
+    document.getElementById('selectNo').innerText=selectNo;
+
 }
