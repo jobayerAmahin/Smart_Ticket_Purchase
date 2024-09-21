edba024085a2f62c1d469313663326869b18de66
jobayerAmahin <ja.mahinmolla.2824@gmail.com>
2024-09-21 23:52:02 +0300

    Adding project Smart Ticketting with new html and utility file with price List

--- utility.js
+++ utility.js
@@ -1,8 +1,25 @@
 //
+let collectedSeat=[];
+let totalPrice=0;
+
 function addSeat(event){
+    if(collectedSeat.length>3){
+        alert('Maximum 4 seats can be selected')
+        event.style.backgroundColor='default';
+        event.style.color='default';
+        return
+    }
+    
     event.style.backgroundColor='lime';
     event.style.color='white';
+
     const seatNo=event.innerText;
+    if(collectedSeat.includes(seatNo)){
+        alert('Seat is already selected');
+        return;
+    }
+    collectedSeat.push(seatNo);
+
     document.getElementById('noSeatLine').classList.add('hidden');
     const newline=document.createElement('div');
     newline.classList.add('flex')
@@ -21,5 +38,16 @@ function addSeat(event){
     let selectNo=parseFloat(document.getElementById('selectNo').innerText)
     selectNo+=1;
     document.getElementById('selectNo').innerText=selectNo;
+
+    totalPrice+=550;
+    const bottomSec=document.createElement('div');
+    bottomSec.classList.add('flex')
+    bottomSec.classList.add('justify-end')
+    bottomSec.innerHTML=`
+    <hr>
+    <p>Total Price</p>
+    <p>${totalPrice}</p>
+    `
+    document.getElementById('totalList').appendChild(bottomSec)
 
 }
